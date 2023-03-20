7f4ea22462ad7e72ec03aaa366ef970a6307d9a5
rico020202 <ricoroca2@gmail.com>
2023-03-20 16:48:35 -0600

    cambiar navbar

--- onyx/controllers/onyx.js
+++ onyx/controllers/onyx.js
@@ -6,6 +6,7 @@ exports.getCatEjercicios = (req, res, next) => {
             res.render('catEjercicios', {
                 ejercicio: rows,
                 pagetitle: 'Catálogo de Ejercicios',
+                user: req.session.user || '',
                 path: '/catEjercicios'
             });
         })
@@ -13,30 +14,51 @@ exports.getCatEjercicios = (req, res, next) => {
 };
 
 exports.getHome = (req, res, next) => {
-    res.render('home', { pagetitle: 'Onyx'});
+    res.render('home', { 
+        pagetitle: 'Onyx',
+        user: req.session.user || '',
+    });
 };
 
 exports.getAdminDashboard = (req, res, next) => {
-    res.render('admindashboard', { pagetitle: 'Onyx'});
+    res.render('admindashboard', { 
+        pagetitle: 'Onyx',
+        user: req.session.user || '',
+    });
 }
 
 
 exports.getCatEntrenamientos = (req, res, next) => {
-    res.render('catEntrenamientos', { pagetitle: 'Catálogo de Entrenamientos'});    
+    res.render('catEntrenamientos', { 
+        pagetitle: 'Catálogo de Entrenamientos',
+        user: req.session.user || '',
+    });    
 }
 
 exports.getDietasparaTi = (req, res, next) => {
-    res.render('dietasParaTi', { pagetitle: 'Dietas para ti'});    
+    res.render('dietasParaTi', { 
+        pagetitle: 'Dietas para ti',
+        user: req.session.user || '',
+    });    
 }
 
 exports.getDietas = (req, res, next) => {
-    res.render('dietas', { pagetitle: 'Dietas'});    
+    res.render('dietas', { 
+        pagetitle: 'Dietas',
+        user: req.session.user || '',
+    });    
 }
 
 exports.getBitacora = (req, res, next) => {
-    res.render('bitacora', { pagetitle: 'Bitácora'});    
+    res.render('bitacora', { 
+        pagetitle: 'Bitácora',
+        user: req.session.user || '',
+    });    
 }
 
 exports.getFavoritos = (req, res, next) => {
-    res.render('favoritos', { pagetitle: 'Favoritos'});    
+    res.render('favoritos', { 
+        pagetitle: 'Favoritos',
+        user: req.session.user || '',
+    });    
 }
