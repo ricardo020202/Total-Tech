c8eb8f741e534db97d183d1a89f41941a6c1b44a
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-03-23 09:44:41 -0600

    models dietas

--- onyx/models/dietas.js
+++ onyx/models/dietas.js
@@ -2,8 +2,8 @@ const db = require('../util/database');
 
 module.exports = class Dieta {
     constructor(dieta) {
-        this.nombre = dieta.nombre;
-        this.calorias = dieta.calorias;
+        this.nombre_dieta = dieta.nombre_dieta;
+        this.no_calorias = dieta.no_calorias;
         this.proteinas = dieta.proteinas;
         this.carbohidratos = dieta.carbohidratos;
         this.grasas = dieta.grasas;
@@ -32,13 +32,29 @@ module.exports = class Dieta {
     }
 
     save() {
-        return db.execute(`INSERT INTO dieta (nombre, calorias, proteinas, carbohidratos, grasas, fibra_total, ceniza, calcio, fosforo, hierro, tiamina, riboflavina, niacina, vitamina_c, vitamina_a, ac_graso_mono, ac_graso_poli, ac_graso_saturado, colesterol, potasio, sodio, zinc, magnesio, vit_b6, vit_b12, ac_folico, folato) 
+        return db.execute(`INSERT INTO dieta (nombre_dieta, no_calorias, proteinas, carbohidratos, grasas, fibra_total, ceniza, calcio, fosforo, hierro, tiamina, riboflavina, niacina, vitamina_c, vitamina_a, ac_graso_mono, ac_graso_poli, ac_graso_saturado, colesterol, potasio, sodio, zinc, magnesio, vit_b6, vit_b12, ac_folico, folato) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?, ?, ? ,?)`,
-            [this.nombre, this.calorias, this.proteinas, this.carbohidratos, this.grasas, this.fibra_total, this.ceniza, this.calcio, this.fosforo, this.hierro, this.tiamina, this.riboflavina, this.niacina, this.vitamina_c, this.vitamina_a, this.ac_graso_mono, this.ac_graso_poli, this.ac_graso_saturado, this.colesterol, this.potasio, this.sodio, this.zinc, this.magnesio, this.vit_b6, this.vit_b12, this.ac_folico, this.folato]
+            [this.nombre_dieta, this.no_calorias, this.proteinas, this.carbohidratos, this.grasas, this.fibra_total, this.ceniza, this.calcio, this.fosforo, this.hierro, this.tiamina, this.riboflavina, this.niacina, this.vitamina_c, this.vitamina_a, this.ac_graso_mono, this.ac_graso_poli, this.ac_graso_saturado, this.colesterol, this.potasio, this.sodio, this.zinc, this.magnesio, this.vit_b6, this.vit_b12, this.ac_folico, this.folato]
         );
     }
 
     static fetchAll() {
         return db.execute('SELECT * FROM dieta');
+    }
+
+    static fetchById(id_dieta)
+    {
+        return db.execute('SELECT * FROM dieta WHERE id_dieta = ?', [id_dieta]);
+    }
+
+    update() {
+        return db.execute(
+            'UPDATE dieta SET nombre_dieta = ?, no_calorias = ?, proteinas = ?, carbohidratos = ?, grasas = ?, fibra_total = ?, ceniza = ?, calcio = ?, fosforo = ?, hierro = ?, tiamina = ?, riboflavina = ?, niacina = ?, vitamina_c = ?, vitamina_a = ?, ac_graso_mono = ?, ac_graso_poli = ?, ac_graso_saturado = ?, colesterol = ?, potasio = ?, sodio = ?, zinc = ?, magnesio = ?, vit_b6 = ?, vit_b12 = ?, ac_folico = ?, folato = ?'
+            [this.nombre_dieta, this.no_calorias, this.proteinas, this.carbohidratos, this.grasas, this.fibra_total, this.ceniza, this.calcio, this.fosforo, this.hierro, this.tiamina, this.riboflavina, this.niacina, this.vitamina_c, this.vitamina_a, this.ac_graso_mono, this.ac_graso_poli, this.ac_graso_saturado, this.colesterol, this.potasio, this.sodio, this.zinc, this.magnesio, this.vit_b6, this.vit_b12, this.ac_folico, this.folato]
+        );
+    }
+
+    static deleteById(id_dieta) {
+        return db.execute('DELETE FROM dieta WHERE id_dieta = ?', [id_dieta]);
     }
 }
