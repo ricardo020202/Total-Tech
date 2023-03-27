56b0c5d9f78ba89a8161b9e2757562d13a7a1d99
Pablo Javier Arreola Velasco <A01747824@tec.mx>
2023-03-27 12:08:45 -0600

    Database

--- onyx/util/database.js
+++ onyx/util/database.js
@@ -1,20 +1,14 @@
 const mysql = require('mysql2');
 
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'onyx',
-//     password: '',
-// });
-
-
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    port: 8889,
     database: 'onyx',
-    password: 'lol23',
+    password: '',
 });
+
+
+
 
 
 module.exports = pool.promise();
