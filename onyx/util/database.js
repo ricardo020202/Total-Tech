059c3a32255fe674373dcc90516323bb559db90b
Paco <paco.couttolenc115@gmail.com>
2023-04-11 18:18:14 -0600

    Configurar base de datos

--- onyx/util/database.js
+++ onyx/util/database.js
@@ -17,7 +17,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'onyx1',
+    database: 'onyx',
     password: '',
 });
 
