3515ca662bffe2542bb96d6c70de9ea13b432ed3
Dante <ddperezp03@gmail.com>
2023-04-11 19:40:10 -0600

    modificar cuenta de cliente

--- onyx/util/database.js
+++ onyx/util/database.js
@@ -15,24 +15,23 @@ const mysql = require('mysql2');
 
 //  Erik database
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'erik',
-    database: 'onyx',
-    password: 'erik',
-    port: 8889,
-});
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'erik',
+//     database: 'onyx',
+//     password: 'erik',
+//     port: 8889,
+// });
 
 // Windows database
 
-/*
+
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'onyx',
     password: '',
 });
-*/
 
 
 
