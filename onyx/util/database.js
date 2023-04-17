94c092527d13782d62d58efb95a9250ad5dcc5e8
bromaeg <a01709653@tec.mx>
2023-04-17 13:57:00 -0500

    Adicion de animaciones en hover para la sidebar de administrador

--- onyx/util/database.js
+++ onyx/util/database.js
@@ -51,7 +51,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'onyx2',
+    database: 'onyx',
     password: '',
 });
 
