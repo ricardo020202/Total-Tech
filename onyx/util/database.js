b6e8d4ebdf49b5b267793890d7bc0c5c6f324b02
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-04-17 09:53:04 -0600

    arreglar lo de Ricardo Rosales

--- onyx/util/database.js
+++ onyx/util/database.js
@@ -3,20 +3,20 @@ const mysql = require('mysql2');
 //Switch as necesary
 
 // Mac database
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'Onyx',
-//     port: 8889,
-//     password: 'root',
-// });
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'Onyx',
     port: 8889,
-    password: 'lol23',
+    password: 'root',
 });
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'Onyx',
+//     port: 8889,
+//     password: 'lol23',
+// });
 //  Erik database
 // const pool = mysql.createPool({
 //     host: 'localhost',
