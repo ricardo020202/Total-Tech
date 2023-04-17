8e33cd5363e14d056131e79767b908d6885bfb3f
bromaeg <a01709653@tec.mx>
2023-04-17 14:51:34 -0500

    Acomodo de las tablas en diseño

--- onyx/util/database.js
+++ onyx/util/database.js
@@ -46,7 +46,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'onyx2',
+    database: 'onyx',
     password: '',
 });
 
