144534fce64e504da4e29d107bdd8edfc6742100
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-04-18 11:04:13 -0600

    listo

--- onyx/util/database.js
+++ onyx/util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 //Switch as necesary
 
 // Mac database
-/* const pool = mysql.createPool({
+/*const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'Onyx',
