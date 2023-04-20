3265f1ede907fb0925849f2245345dd1ce62434b
rico020202 <ricoroca2@gmail.com>
2023-04-19 21:14:05 -0600

    Agregar privilegios a base y funcion de validar privilegios

--- onyx/routes/admin.js
+++ onyx/routes/admin.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const checkPrivilegio = require("../util/checkPrivilegios");
 
 const adminController = require("../controllers/admin");
 
--- onyx/routes/onyx.js
+++ onyx/routes/onyx.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const onyxController = require("../controllers/onyx");
+const checkPrivilegio = require("../util/checkPrivilegios");
 
 router.get("/catejercicios", onyxController.getCatEjercicios);
 
