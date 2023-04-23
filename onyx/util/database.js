1678c9c6c5c363d35f6421cd80546ce881fcbf56
rico020202 <ricoroca2@gmail.com>
2023-04-23 12:39:07 -0600

    Corregir error admin roles y agregar googler auth

--- onyx/util/database.js
+++ onyx/util/database.js
@@ -10,13 +10,13 @@ const mysql = require('mysql2');
 //     port: 8889,
 //     password: 'root',
 // });
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'Onyx',
-    port: 8889,
-    password: 'lol23',
-});
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'Onyx',
+//     port: 8889,
+//     password: 'lol23',
+// });
 //  Erik database
 // const pool = mysql.createPool({
 //     host: 'localhost',
@@ -34,12 +34,12 @@ const pool = mysql.createPool({
 });*/
 
 // Windows database
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'onyx2',
-//     password: '',
-// });
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'onyx2',
+    password: '',
+});
 // const pool = mysql.createPool({
 //     host: 'localhost',
 //     user: 'root',
