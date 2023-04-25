acd755d42907597e93f0b3f2e511e790a3f8ebbb
ricardo020202 <ricoroca2@hotmail.com>
2023-04-25 10:07:29 -0600

    Eliminar segunda conexion a db

--- onyx/controllers/googleAuthController.js
+++ onyx/controllers/googleAuthController.js
@@ -1,19 +1,12 @@
 /*googleAuthController.js*/
 
 const { OAuth2Client } = require('google-auth-library');
-const mysql = require('mysql2');
 const CLIENT_ID = '141860110384-4rkak3dc4oehtdj46ogd2aufa9vf34le.apps.googleusercontent.com';
 const CLIENT_SECRET = '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';
 const REDIRECT_URI = 'http://localhost:3000/onyx';
 const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
-// Configura tu conexión a la base de datos aquí
-const connection = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: '',
-database: 'onyx'
-});
+const db = require("../util/database");
 
 const googleAuthController = {};
 
