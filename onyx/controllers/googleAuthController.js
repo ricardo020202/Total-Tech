be25e395680ed15cf8307293c76f7d7b6ee57b6c
ricardo020202 <ricoroca2@hotmail.com>
2023-04-25 10:20:12 -0600

    Eliminar segunda conexion a db

--- onyx/controllers/googleAuthController.js
+++ onyx/controllers/googleAuthController.js
@@ -12,37 +12,37 @@ const googleAuthController = {};
 
 googleAuthController.callbackPost = async (req, res) => {
 
-// Obtén el código de autorización de la URL
-const code = req.query.code;
+    // Obtén el código de autorización de la URL
+    const code = req.query.code;
 
-if (!code) {
-res.status(400).send('No se proporcionó el código de autorización.');
-return;
-}
+    if (!code) {
+        res.status(400).send('No se proporcionó el código de autorización.');
+        return;
+    }
 
-try {
-// Intercambia el código de autorización por tokens de acceso y de ID
-const { tokens } = await oAuth2Client.getToken(code);
-oAuth2Client.setCredentials(tokens);
+    try {
+        // Intercambia el código de autorización por tokens de acceso y de ID
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
 
-// Usa el token de ID para verificar la autenticación y obtener la información del usuario
-const ticket = await oAuth2Client.verifyIdToken({
-idToken: tokens.id_token,
-audience: CLIENT_ID,
-});
-const payload = ticket.getPayload();
-const userId = payload.sub;
-const userEmail = payload.email;
+        // Usa el token de ID para verificar la autenticación y obtener la información del usuario
+        const ticket = await oAuth2Client.verifyIdToken({
+            idToken: tokens.id_token,
+            audience: CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+        const userId = payload.sub;
+        const userEmail = payload.email;
 
-// Aquí puedes manejar la autenticación de tu aplicación y vincular el usuario de Google con tu aplicación
-// Por ejemplo, puedes crear o actualizar el usuario en tu base de datos utilizando 'userId' y 'userEmail'
+        // Aquí puedes manejar la autenticación de tu aplicación y vincular el usuario de Google con tu aplicación
+        // Por ejemplo, puedes crear o actualizar el usuario en tu base de datos utilizando 'userId' y 'userEmail'
 
-// ... (El código de manejo de la base de datos se mantiene igual que antes)
+        // ... (El código de manejo de la base de datos se mantiene igual que antes)
 
-} catch (error) {
-console.error('Error al intercambiar el código de autorización por tokens:', error);
-res.status(500).send('Error en el proceso de autenticación.');
-}
+    } catch (error) {
+        console.error('Error al intercambiar el código de autorización por tokens:', error);
+        res.status(500).send('Error en el proceso de autenticación.');
+    }
 };
 
 module.exports = googleAuthController;
