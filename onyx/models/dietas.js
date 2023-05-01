7186a143f1fc740d65332d8a2cade5510e3241c5
rico020202 <ricoroca2@gmail.com>
2023-04-30 19:16:31 -0600

    Otra psoible solucion a error dietas en servidor

--- onyx/models/dietas.js
+++ onyx/models/dietas.js
@@ -59,10 +59,6 @@ module.exports = class Dieta {
     }
 
     static fetchByCal(numcal, start) {
-        if (start == 0) {
-            return db.execute('SELECT * FROM dieta ORDER BY calorias  LIMIT 0, 9');
-        }
-
         if (numcal == 0) {
             return db.execute('SELECT * FROM dieta ORDER BY calorias  LIMIT ?, 9', [start]);
         }
